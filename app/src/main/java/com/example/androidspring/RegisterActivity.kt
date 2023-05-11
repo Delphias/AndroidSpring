6caeb7ed5375d44e2efa09e13297562e79cc3e5d
Delphias <sekhukhunedelphia@gmail.com>
2023-05-11 16:34:54 +0200

    Added the registerUser method in the registerActivity and permissions in the AndroidManifest.xml to connect to the internet

--- app/src/main/java/com/example/androidspring/RegisterActivity.kt
+++ app/src/main/java/com/example/androidspring/RegisterActivity.kt
@@ -1,5 +1,6 @@
 package com.example.androidspring
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
@@ -14,35 +15,42 @@ import java.io.IOException
 
 class RegisterActivity : AppCompatActivity() {
 // lateinit allows us to avoid initializing variables when a constructor is  build/created (using the this. keyword to initialize them
+    private lateinit var password: String
+    private lateinit var userName: String
     lateinit var etUsername: EditText
     lateinit var etPassword: EditText
     lateinit var buttonRegister: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
-//  the super class keyword calls the superclass constructor and should be the executed first
+//  the super class keyword calls the superclass constructor and should be executed first
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register2)
 
-//finding the assigned values of the set variables
+//findViewById binds a view (textview, buttons, checkbox) in an Android layout(the .xml file) to a field in an Android activity or fragment (the class).
         etUsername= findViewById(R.id.etRUserName)
         etPassword= findViewById(R.id.etRPassword)
         buttonRegister= findViewById(R.id.btnRegister)
 
+// buttonRegister.setOnClickListener{
+// declare as a global variable when testing with Toast
+// lateinit var userName : String
+// It gets user input and store it in the variable userName
+// var userName = etUsername.text.toString()
+// Used to display a short time notification to the user without affecting the user interaction with UI. The message displayed using Toast class displays quickly, and it disappears after some time
+// Toast.makeText(this,  userName + " has been registered", Toast.LENGTH_SHORT).show();
 
+
+// setting up the button to listen when it's clicked.
         buttonRegister.setOnClickListener{
-//            lateinit var userName : String --> declare as a global variable when using Toast
-//            var userName = etUsername.text.toString()
-//            Toast.makeText(this,  userName + " has been registered", Toast.LENGTH_SHORT).show();
-
+// calling the method registerUser
             registerUser()
-
         }
     }
 
     private fun registerUser() {
 
-        val userName: String = etUsername.getText().toString().trim()
-        val password: String = etPassword.getText().toString().trim()
+//        val userName: String = etUsername.getText().toString().trim()
+//        val password: String = etPassword.getText().toString().trim()
         if (userName.isEmpty()) {
             etUsername.setError("Username is required")
             etUsername.requestFocus()
@@ -65,11 +73,23 @@ class RegisterActivity : AppCompatActivity() {
                     e.printStackTrace()
                 }
 
+                if (s == "SUCCESS") {
+                    Toast.makeText(
+                        this@RegisterActivity,
+                        "Successfully registered. Please login",
+                        Toast.LENGTH_LONG
+                    ).show()
+                    startActivity(Intent(this@RegisterActivity, LoginActivity::class.java))
+                } else {
+                    Toast.makeText(this@RegisterActivity, "User already exists!", Toast.LENGTH_LONG)
+                        .show()
+                }
             }
 
             override fun onFailure(call: Call<ResponseBody?>, t: Throwable) {
-                TODO("Not yet implemented")
+                Toast.makeText(this@RegisterActivity, t.message, Toast.LENGTH_LONG).show()
             }
         })
+
 }
 }
