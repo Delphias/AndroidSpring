fd2c77b5dc0b3686bd424475a9da07744a2cfa3e
Delphias <sekhukhunedelphia@gmail.com>
2023-05-12 16:30:15 +0200

    Added Splash to the Homepage before navigating to the LoginActivity

--- app/src/main/java/com/example/androidspring/MainActivity.kt
+++ app/src/main/java/com/example/androidspring/MainActivity.kt
@@ -1,14 +1,28 @@
 package com.example.androidspring
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_main)
-        setContentView(R.layout.activity_register2)
+        setContentView(R.layout.activity_main)
+
+        Handler().postDelayed({
+            val intent = Intent(this@MainActivity, LoginActivity::class.java)
+            startActivity(intent)
+
+                              }, 3000)
+
+
+
+
+//        setContentView(R.layout.activity_register2)
+
+//        supportActionBar?.hide
 
 
 
